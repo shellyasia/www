48d0001e2df565291c29537fbc5e990a393518d8
Eric Zhou <eric.zhou@shelly.com>
2025-09-11 21:20:53 +0800

    Enhance mobile responsiveness and accessibility with CSS adjustments

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -468,11 +468,10 @@ export default defineConfig({
       { icon: 'shopee', link: 'https://shellycloud.en.alibaba.com' },
       { icon: 'shopify', link: 'https://mall.jd.com/index-20140293.html' },
       { icon: 'github', link: 'https://github.com/shellyasia/www.git' },
-
-
     ],
   },
   head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' }],
     ['meta', { name: 'algolia-site-verification', content: algoliaContent }],
     [
       'meta',
